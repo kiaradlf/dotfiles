32abea3341c087f0956c22285078cd69534896ce
kiaradlf <166040867+kiaradlf@users.noreply.github.com>
2024-04-11 09:24:00 +0200

    Update setup.sh - add docker

--- setup.sh
+++ setup.sh
@@ -3,8 +3,21 @@
 # install Nix: https://nix.dev/install-nix
 curl -L https://nixos.org/nix/install | sh -s -- --daemon
 
+# install docker: https://docs.docker.com/engine/install/debian/
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # add existing ssh keys
-chmod 0600 ~/.ssh/*
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_auth
 ssh-add ~/.ssh/id_sign
