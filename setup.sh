9cea6c383541b4a50daca2084cdb576e959fb7d0
kiaradlf <166040867+kiaradlf@users.noreply.github.com>
2024-04-11 11:38:38 +0200

    Update setup.sh - docker group

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $(whoami)
 
 # install vscode: https://code.visualstudio.com/blogs/2020/12/03/chromebook-get-started
 sudo apt-get update
